a5148c687a484e5278444c2898e3be5c4a665922
Tanita Mošić <tanitamosic@gmail.com>
2022-11-11 11:25:31 +0100

    Initialized project

---
+++ src/main/java/com/NWT_KTS_project/NwtKtsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.NWT_KTS_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwtKtsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwtKtsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NWT_KTS_project/NwtKtsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NWT_KTS_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwtKtsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
