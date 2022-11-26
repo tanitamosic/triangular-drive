2f2ff0a986ec5b1b0a4ca433c2bc2a14f0adf782
Tanita Mošić <tanitamosic@gmail.com>
2022-11-26 14:49:33 +0100

    Reconfigured application, added dependencies

--- src/main/java/com/NWT_KTS_project/NwtKtsProjectApplication.java
+++ src/main/java/com/NWT_KTS_project/NwtKtsProjectApplication.java
@@ -1,12 +1,15 @@
-package com;
+package com.NWT_KTS_project;
 
+import com.NWT_KTS_project.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class NwtKtsProjectApplication {
+import java.util.Scanner;
 
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}, scanBasePackages = {"com"})
+public class NwtKtsProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NwtKtsProjectApplication.class, args);
 	}
