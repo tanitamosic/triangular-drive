0d7618d5c2266f19962f27d529fbae09e92fefb1
Petar Kupusarević <petar.kupusarevic@gmail.com>
2023-01-09 12:51:15 +0100

    Implemented sending reviews for Clients

--- src/main/java/com/NWT_KTS_project/controllers/ClientController.java
+++ src/main/java/com/NWT_KTS_project/controllers/ClientController.java
@@ -2,6 +2,8 @@ package com.NWT_KTS_project.controllers;
 
 
 
+import com.NWT_KTS_project.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -10,7 +12,13 @@ import org.springframework.web.bind.annotation.*;
 public class ClientController {
 
 
+    @Autowired
+    private ReviewService reviewService;
 
+    @PostMapping("reviewRide/{rideId}/{userId}")
+    void reviewRide(@PathVariable Integer rideId, @PathVariable Integer userId, @RequestBody String comment, @RequestBody Integer carRating, @RequestBody Integer driverRating){
+        reviewService.sendRideReview(rideId, userId, carRating, driverRating, comment);
+    }
 
 
 
