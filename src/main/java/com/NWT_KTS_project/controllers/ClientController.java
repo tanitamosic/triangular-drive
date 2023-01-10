8b2d6bba0b59016ac550b123e3ecfc0c1c003950
Petar Kupusarević <petar.kupusarevic@gmail.com>
2023-01-10 13:16:27 +0100

    Implemented finding the closest available driver with respect to pet/baby friendly preferences

--- src/main/java/com/NWT_KTS_project/controllers/ClientController.java
+++ src/main/java/com/NWT_KTS_project/controllers/ClientController.java
@@ -2,7 +2,10 @@ package com.NWT_KTS_project.controllers;
 
 
 
+import com.NWT_KTS_project.model.users.Driver;
+import com.NWT_KTS_project.service.DriverService;
 import com.NWT_KTS_project.service.ReviewService;
+import com.NWT_KTS_project.service.RideService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +18,25 @@ public class ClientController {
     @Autowired
     private ReviewService reviewService;
 
+    @Autowired
+    private DriverService driverService;
+
+    @Autowired
+    private RideService rideService;
+
     @PostMapping("reviewRide/{rideId}/{userId}")
     void reviewRide(@PathVariable Integer rideId, @PathVariable Integer userId, @RequestBody String comment, @RequestBody Integer carRating, @RequestBody Integer driverRating){
         reviewService.sendRideReview(rideId, userId, carRating, driverRating, comment);
     }
 
 
+    @GetMapping("requestRide/{userId}/{longitude}/{latitude}")
+    int requestRide(@PathVariable Integer userId, @PathVariable Double longitude, @PathVariable Double latitude,@RequestBody String passengers,@RequestBody boolean petFriendly, @RequestBody boolean babyFriendly){
+        int numPassengers = passengers.split(";").length;
+        Driver driver = driverService.getAvailableDriver(latitude, longitude, petFriendly, babyFriendly, numPassengers);
+        if(driver == null) return -1;
 
+    }
 
 
 
--- src/main/java/com/NWT_KTS_project/service/PositionService.java
+++ src/main/java/com/NWT_KTS_project/service/PositionService.java
@@ -28,5 +28,27 @@ public class PositionService {
     }
 
 
+    public double getDistance(Position p1, Position p2) {
+        double lat1 = p1.getLatitude();
+        double lat2 = p2.getLatitude();
+        double lon1 = p1.getLongitude();
+        double lon2 = p2.getLongitude();
+
+        double R = 6371e3;
+        double phi1 = lat1 * Math.PI/180;
+        double phi2 = lat2 * Math.PI/180;
+        double deltaPhi = (lat2-lat1) * Math.PI/180;
+        double deltaLambda = (lon2-lon1) * Math.PI/180;
+
+        double a = Math.sin(deltaPhi/2) * Math.sin(deltaPhi/2) +
+                Math.cos(phi1) * Math.cos(phi2) *
+                        Math.sin(deltaLambda/2) * Math.sin(deltaLambda/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return R * c;//meters
+    }
+
+    public static final double MAX_DISTANCE = 2000;
+
 
 }
