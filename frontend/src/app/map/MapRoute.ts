9a42d257f83ff45fd53a6814d549ddaaa09d57b1
Mihajlo Djordjevic <mihajlodjordjevic931@gmail.com>
2023-01-25 21:27:14 +0100

    Added input of destinations and drawing map routes between them.

---
+++ frontend/src/app/map/MapRoute.ts
@@ -0,0 +1,21 @@
+export class Stop {
+  x: number = 0;
+  y: number = 0;
+  address: String = '';
+}
+
+export class MapRoute {
+  stops: Stop[] = [];
+
+  getAddresses(): String[] {
+    let retval: String[] = [];
+    this.stops.forEach(s => {
+      retval.push(s.address);
+    });
+    return retval;
+  }
+
+  addStop(s: Stop) {
+    this.stops.push(s);
+  }
+}
--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -2,6 +2,9 @@ import { Component,  AfterViewInit } from '@angular/core';
 import 'leaflet';
 import 'leaflet-routing-machine';
 import {GeoSearchControl, OpenStreetMapProvider} from 'leaflet-geosearch';
+import "node_modules/leaflet-geosearch/dist/geosearch.css"
+import {MapRoute, Stop} from "./MapRoute";
+import {SearchResult} from "leaflet-geosearch/lib/providers/provider";
 
 declare var L: any;
 
@@ -13,19 +16,28 @@ declare var L: any;
 export class MapComponent implements AfterViewInit  {
 
   private map: any;
+  inputCounter: number = 0;
+
+  start: String = '';
+  dest1: String = '';
+  dest2: String = '';
+  dest3: String = '';
+  dest4: String = '';
+  final: String = '';
 
   provider: OpenStreetMapProvider;
   searchControl: any;
+
+  mapRoute: MapRoute;
+
   constructor() {
     this.provider = new OpenStreetMapProvider();
-    // @ts-ignore
-    this.searchControl = new GeoSearchControl({
-      provider: this.provider,
-    });
+    this.mapRoute = new MapRoute();
   }
 
   ngAfterViewInit(): void {
     this.initMap();
+
   }
 
   private initMap(): void {
@@ -40,26 +52,93 @@ export class MapComponent implements AfterViewInit  {
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
-
-    L.Routing.control({
-      waypoints: [
-        L.latLng(45.257795, 19.834442),
-        L.latLng(45.2361365, 19.838948636610578)
-      ],
-      routeWhileDragging: true
-    }).addTo(this.map);
     tiles.addTo(this.map);
-    this.drawMarker();
-
-    this.search();
-
   }
 
    search() {
-    this.provider.search({ query: "Bulevar Despota Stefana 7, 21000 Novi Sad, Republika Srbija" })
-      .then((result) => {
-        console.log(result);
+      if ((this.start === '' || this.final === '') || this.start.length < 10 || this.final.length < 10) {
+        alert('Your route needs beginning and the end, dummy.')
+        return;
+      }
+      let previousStop = {x: 0, y: 0};
+      this.getQueryResult(this.start).then(r => {
+        previousStop.x = r[0].x;
+        previousStop.y = r[0].y;
+
+        this.addStopToRoute(r);
       });
+
+      if (this.dest1 !== '' && this.dest1.length > 10) {
+        this.getQueryResult(this.dest1).then(r => {
+          this.drawRoute(previousStop.x, previousStop.y, r[0].x, r[0].y);
+          previousStop.x = r[0].x;
+          previousStop.y = r[0].y;
+
+          this.addStopToRoute(r);
+        });
+      }
+
+       if (this.dest2 !== '' && this.dest2.length > 10) {
+         this.getQueryResult(this.dest2).then(r => {
+           this.drawRoute(previousStop.x, previousStop.y, r[0].x, r[0].y);
+           previousStop.x = r[0].x;
+           previousStop.y = r[0].y;
+
+           this.addStopToRoute(r);
+         });
+       }
+
+       if (this.dest3 !== '' && this.dest3.length > 10) {
+          this.getQueryResult(this.dest3).then(r => {
+            this.drawRoute(previousStop.x, previousStop.y, r[0].x, r[0].y);
+            previousStop.x = r[0].x;
+            previousStop.y = r[0].y;
+
+            this.addStopToRoute(r);
+          });
+       }
+
+       if (this.dest4 !== '' && this.dest4.length > 10) {
+          this.getQueryResult(this.dest4).then(r => {
+            this.drawRoute(previousStop.x, previousStop.y, r[0].x, r[0].y);
+            previousStop.x = r[0].x;
+            previousStop.y = r[0].y;
+
+            this.addStopToRoute(r);
+          });
+       }
+       this.getQueryResult(this.final).then(r => {
+         this.drawRoute(previousStop.x, previousStop.y, r[0].x, r[0].y);
+         previousStop.x = r[0].x;
+         previousStop.y = r[0].y;
+
+         this.addStopToRoute(r);
+       });
+  }
+
+  private addStopToRoute(r: SearchResult<Object>[]) {
+    let newStop = new Stop();
+    newStop.x = r[0].x;
+    newStop.y = r[0].y;
+    newStop.address = r[0].label;
+    this.mapRoute.addStop(newStop);
+  }
+
+  async getQueryResult(query: String) {
+    const result = await this.provider.search({query: String(query)})
+    console.log(result);
+    return result;
+  }
+
+  drawRoute(x1: number, y1: number, x2: number, y2: number) {
+    L.Routing.control({
+      waypoints: [
+        L.latLng(y1, x1),
+        L.latLng(y2, x2)
+      ],
+
+      routeWhileDragging: false
+    }).addTo(this.map);
   }
 
 
@@ -79,11 +158,26 @@ export class MapComponent implements AfterViewInit  {
   }
 
 
+  addDest() {
+    if (this.inputCounter === 4) { return; }
+    this.inputCounter = this.inputCounter + 1;
+  }
 
-
-
-
-
+  removeDest() {
+    if (this.inputCounter === 4) {
+      this.dest4 = '';
+      this.inputCounter -= 1;
+    } else if (this.inputCounter === 3) {
+      this.dest3 = '';
+      this.inputCounter -= 1;
+    } else if (this.inputCounter === 2) {
+      this.dest2 = '';
+      this.inputCounter -= 1;
+    } else if (this.inputCounter === 1) {
+      this.dest1 = '';
+      this.inputCounter -= 1;
+    }
+  }
 }
 
 
